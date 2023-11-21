8137890f8333de2d3f5ffbad5ef56984383a1f33
Joed Machado <joedmg58@gmail.com>
2023-11-21 16:54:08 -0500

    Update from office

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,20 @@
 JsBarcode(".barcode", '123456', {height: 40, displayValue: false}).init();
+
+//Menu toggle
+
+const toggle = document.getElementById('menu-toggle');
+const sideBar = document.querySelector('.side-bar');
+const main = document.querySelector('.main');
+
+toggle.onclick = function() {
+    sideBar.classList.toggle('menu-toggle-active');
+    main.classList.toggle('menu-toggle-active');
+}
+
+function deleteUser(e) {
+    console.log(e);
+}
+
+function editUser(e) {
+
+}
