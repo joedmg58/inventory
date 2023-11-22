dceb3b34a8ae7e4d5524dce29f1d8b3bc46aaaf0
Joed Machado <joedmg58@gmail.com>
2023-11-22 12:45:26 -0500

    Update from office

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,9 @@
+//Initialize JsBarCode
 JsBarcode(".barcode", '123456', {height: 40, displayValue: false}).init();
+
+//Initialize BS Tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 //Menu toggle
 
@@ -11,10 +16,36 @@ toggle.onclick = function() {
     main.classList.toggle('menu-toggle-active');
 }
 
+//Users
+
 function deleteUser(userId) {
-    alert(`Deleting userid ${userId}`);
+    // alert(`Deleting userid ${userId}`);
+    const deleteUserModal = new bootstrap.Modal( '#delete-user-modal' );
+    deleteUserModal.show();
 }
 
-function editUser(userId) {
-    alert(`Editing userid ${userId}`);
+function editUser(e) {
+    const user = {
+        id: e.dataset.userId,
+        name: e.dataset.userName,
+        email: e.dataset.userEmail
+    }
+
+    const editUserEl = document.querySelector('#edit-user');
+    editUserEl.dataset.userId = user.id;
+    const editUserNameEl = document.querySelector('#edit-user-name');
+    editUserNameEl.value = user.name;
+    const editUserEmailEl = document.querySelector('#edit-user-email');
+    editUserEmailEl.value = user.email;
+
+    const editUserModal = new bootstrap.Modal( '#edit-user-modal' );
+    editUserModal.show();
+}
+
+function fetchEditUser(e) {
+    e.preventDefault();
+
+    const editUserEl = document.querySelector('#edit-user');
+
+    console.log(editUserEl.dataset.userId);
 }
